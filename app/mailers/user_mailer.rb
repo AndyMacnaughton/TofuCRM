3894748ac519a7c7e4ab418167b16efdcb2ae57e
Andy Macnaughton <andy.macnaughton@gmail.com>
2014-08-26 20:43:58 +0100

    fixed issue with mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,12 @@
 class UserMailer < ActionMailer::Base
-  default from: "andy.macnaughton@gmail.com"
-
-  DELIVERY_OPTIONS = { user_name: 'andy.macnaughton@gmail.com',
-                     password: 'macnaua1',
-                     address: 'smtp.gmail.com' }
 
   def password_reset(user)
     @user = user
     @url  = root_url
-    puts @user
 
     mail(
       to: @user.email,
-      subject: 'Tofu Password Reset',
-      delivery_options: DELIVERY_OPTIONS
+      subject: 'Tofu Password Reset'
     )
   end
 
@@ -23,8 +16,7 @@ class UserMailer < ActionMailer::Base
 
     mail(
       to: @user.email,
-      subject: 'Welcome to Tofu Bitch',
-      delivery_options: DELIVERY_OPTIONS
+      subject: 'Welcome to Tofu Bitch'
     )
 
   end
